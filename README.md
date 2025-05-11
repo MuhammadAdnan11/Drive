ea6a6ddd4e07f0b127a72cffbdc3711b8a333005
Adnan khan <adnan191022@gmail.com>
2025-05-11 00:56:34 -0700

    added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Drive
+## npm init -y
+## npm i express ejs
+ejs is use for to eject html from the backend.
+
---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router= express.Router();
+
+// /user/test */
+
+router.get('/register',(req,res)=>{
+    res.render('register');
+})
+
+router.post('/register',(req,res)=>{
+    console.log(req.body);
+    res.send('User Registered')
+})
+
+module.exports=router;
